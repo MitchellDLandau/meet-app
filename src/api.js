bb1e4b2e9ee6942b144783b83394b61537fa3a21
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-10-29 16:40:22 -0400

    trubleshooting
    troubleshooting my api end points.

--- src/api.js
+++ src/api.js
@@ -35,7 +35,7 @@ export const getEvents = async () => {
     const token = await getAccessToken();
     if (token) {
         removeQuery();
-        const url = "https://mitchelldlandau.github.io/meet-app" + "/" + token;
+        const url = "https://7g67xfrpr6.execute-api.us-east-1.amazonaws.com/dev/api/get-events" + "/" + token;
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
