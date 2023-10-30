72669b2902b842eaa8712b743ea96f80a0ce9bea
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-10-30 16:15:35 -0400

    testing
    fixing order of api access functions

--- src/api.js
+++ src/api.js
@@ -1,24 +1,9 @@
 import mockData from './mock-data';
 
-export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
-        await localStorage.removeItem("access_token");
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = await searchParams.get("code");
-        if (!code) {
-            const response = await fetch(
-                "https://7g67xfrpr6.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-            );
-            const result = await response.json();
-            const { authUrl } = result;
-            return (window.location.href = authUrl);
-        }
-        return code && getToken(code);
-    }
-    return accessToken;
+export const extractLocations = (events) => {
+    const extractedLocations = events.map((event) => event.location);
+    const locations = [...new Set(extractedLocations)];
+    return locations;
 };
 
 const checkToken = async (accessToken) => {
@@ -44,6 +29,17 @@ const removeQuery = () => {
     }
 };
 
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+        'https://7g67xfrpr6.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    );
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+
+    return access_token;
+};
+
 export const getEvents = async () => {
     if (window.location.href.startsWith("http://localhost")) {
         return mockData;
@@ -60,15 +56,25 @@ export const getEvents = async () => {
     }
 };
 
-const getToken = async (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-        'https://7g67xfrpr6.execute-api.us-east-1.amazonaws.com/dev/api/token' + '/' + encodeCode
-    );
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
 
-    return access_token;
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+            const response = await fetch(
+                "https://7g67xfrpr6.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+            );
+            const result = await response.json();
+            const { authUrl } = result;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
 };
 
 // export const getAccessToken = async () => {
@@ -139,10 +145,4 @@ const getToken = async (code) => {
 //     access_token && localStorage.setItem("access_token", access_token);
 //     return access_token;
 // };
-
-export const extractLocations = (events) => {
-    const extractedLocations = events.map((event) => event.location);
-    const locations = [...new Set(extractedLocations)];
-    return locations;
-};
 
