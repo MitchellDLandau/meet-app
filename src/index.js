2626ad4eb28a8d11c5f75984eb734d533aabf695
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-11-15 15:49:42 -0500

    removing atatus

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import * as atatus from 'atatus-spa';
-atatus.config('afafd878480c424f967e0399786480b6').install();
+// import * as atatus from 'atatus-spa';
+// atatus.config('afafd878480c424f967e0399786480b6').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
