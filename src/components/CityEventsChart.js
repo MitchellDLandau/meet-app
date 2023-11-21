faeedada4a3752045cf2cb83092e7eee4f07831e
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-11-21 16:05:05 -0500

    city spacing
    Adjusted city spacing on graph.

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -18,7 +18,7 @@ const CityEventsChart = ({ events, allLocations }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
             const count = events.filter((event) => event.location === location).length;
-            const city = location.split(', ')[0];
+            const city = location.split(', ')[0]
             return { city, count };
         })
         return data;
@@ -35,7 +35,8 @@ const CityEventsChart = ({ events, allLocations }) => {
                 }}
             >
                 <CartesianGrid />
-                <XAxis type="category" dataKey="city" name="City" />
+                <XAxis type="category" dataKey="city" name="City"
+                    angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }} />
                 <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
                 <Tooltip cursor={{ strokeDasharray: '3 3' }} />
                 <Scatter name="A school" data={data} fill="#8884d8" />
